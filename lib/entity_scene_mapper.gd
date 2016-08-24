29f534a3ec2c19e1a1cb357a99aabb9f8a74e150
Arron Washington <arron@theradicaledwards.com>
2016-08-24 00:48:23 -0400

    Preload entities, for now.

--- lib/entity_scene_mapper.gd
+++ lib/entity_scene_mapper.gd
@@ -1,12 +1,18 @@
 
 extends Node
 
+const HUMAN = preload("res://entities/human.tscn")
+const FLOORS = preload("res://entities/floors.tscn")
+const PLAYER = HUMAN
+const STRUCTURES = preload("res://entities/structure.tscn")
+const WALLS = preload("res://entities/walls.tscn")
+
 const ENTITY_TYPE_TO_SCENE_MAP = {
-	"human": "res://entities/human.tscn",
-	"floors": "res://entities/floors.tscn",
-	"player": "res://entities/human.tscn",
-	"structures": "res://entities/structure.tscn",
-	"walls": "res://entities/walls.tscn"
+	"human": HUMAN,
+	"floors": FLOORS,
+	"player": PLAYER,
+	"structures": STRUCTURES,
+	"walls": WALLS
 }
 
 func _ready():
@@ -18,6 +24,6 @@ func map(type):
 	if !ENTITY_TYPE_TO_SCENE_MAP.has(type):
 		print("UNKNOWN TYPE: " + type)
 		return
-	return ENTITY_TYPE_TO_SCENE_MAP[type]	
+	return ENTITY_TYPE_TO_SCENE_MAP[type]
 
 
