2c87cea553950e7002f028b4a9cea3c905a4919a
Arron Washington <arron@theradicaledwards.com>
2016-08-26 01:37:46 -0400

    Slightly alter login flow

--- login/Login Screen.gd
+++ login/Login Screen.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
 func _ready():
+	get_node("Panel/LineEdit").grab_focus()
 	set_process_input(true)
 	
-func _process():
-	print("LOL")
-	
+func _input(ev):
+	if ev.is_action_pressed("text_submit"):
+		login(get_node("Panel/LineEdit").get_text())
+
+func login(username):
+	var player_data = {
+		"name": username
+	}
+	var global = get_node("/root/global")
+	global.connectToServer(player_data)
+
+func _on_Button_login(text):
+	login(text)
