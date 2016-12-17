2eb64070c4c1769c550caf36333cda44b3002e4e
Arron Washington <arron@theradicaledwards.com>
2016-12-16 19:22:43 -0500

    Fix up the inventory panel.

--- hud/inventory.gd
+++ hud/inventory.gd
@@ -1,30 +1,25 @@
-extends GridContainer
+extends PanelContainer
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-onready var slot_one = get_node("PanelContainer/ButtonGroup/HBoxContainer/Button")
-onready var slot_two = get_node("PanelContainer/ButtonGroup/HBoxContainer/Button 2")
-onready var slot_three = get_node("PanelContainer/ButtonGroup/HBoxContainer/Button 3")
-onready var slot_four = get_node("PanelContainer/ButtonGroup/HBoxContainer/Button 4")
-
-const CURRENT_SLOT = 0
-const MAX_SLOTS = 4
-
-var slots = []
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	slots = [slot_one, slot_two, slot_three, slot_four]
+	pass
 
 func add_to_inventory(entity_node):
-	if CURRENT_SLOT >= MAX_SLOTS:
-		return
-	var slot = slots[CURRENT_SLOT]
-	
-	slot.set_text(entity_node.get_entity_name())
 	entity_node.hide()
+	var tex = get_frame_texture(entity_node.get_node("Sprite"))
+	get_node("ItemList").add_item(entity_node.get_entity_name(), tex)
+
+func get_frame_texture(sprite):
+	var n = sprite
+	var tex = n.get_texture()
+	var at = AtlasTexture.new()
+	var frame = n.get_frame()
 	
-	CURRENT_SLOT = CURRENT_SLOT + 1
+	var width = tex.get_width() / n.get_hframes()
+	var height = tex.get_height() / n.get_vframes()
+	var x = (frame * width) % tex.get_width()
+	var y = ((frame * width) / tex.get_width() * height)
 	
-	
+	var region = Rect2(x, y, width, height)
+	at.set_atlas(tex)
+	at.set_region(region)
+	return at
